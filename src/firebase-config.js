5421d533914e3c5b4bff70e3959a51f8ab3fa0cd
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-04-23 18:12:47 +0530

    commit

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth , GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAPI-iW-5HkbAXlUfNoMBMnkCZwzgTFjZg",
-    authDomain: "blog-app-react-firebase.firebaseapp.com",
-    projectId: "blog-app-react-firebase",
-    storageBucket: "blog-app-react-firebase.appspot.com",
-    messagingSenderId: "1002591716502",
-    appId: "1:1002591716502:web:894cd983e1047e7b5c2eb7"
-};
+  apiKey: 'AIzaSyAtUlJYfYuG7pqEOnxjqHbkYG7FGo__jBg',
+  authDomain: 'blog-project-using-react.firebaseapp.com',
+  projectId: 'blog-project-using-react',
+  storageBucket: 'blog-project-using-react.appspot.com',
+  messagingSenderId: '100879503113',
+  appId: '1:100879503113:web:f856479c8ad9bd0e20cdd8',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app)
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
